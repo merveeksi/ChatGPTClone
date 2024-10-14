9f51b86f4a3d4184b47e20297a62082644f98c8d
MERVE EKŞİ <merveeksii61@gmail.com>
2024-10-14 20:43:15 +0300

    en güncel

--- ChatGPTClone.WebApi/obj/Debug/net8.0/ChatGPTClone.WebApi.AssemblyInfo.cs
+++ ChatGPTClone.WebApi/obj/Debug/net8.0/ChatGPTClone.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatGPTClone.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53e0a37caf77ac94e709ee79ab1829ff6ef63d51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39541be9ba3523ff3dcc608dc7e0c82428c24ab0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatGPTClone.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatGPTClone.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
