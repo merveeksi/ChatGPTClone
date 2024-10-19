a660adeaa6da77b2c8e27300d6ad3501b732e148
MERVE EKŞİ <merveeksii61@gmail.com>
2024-10-19 21:26:02 +0300

    Blazor Chat UI

--- ChatGPTClone.WebApi/obj/Debug/net8.0/ChatGPTClone.WebApi.AssemblyInfo.cs
+++ ChatGPTClone.WebApi/obj/Debug/net8.0/ChatGPTClone.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatGPTClone.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39541be9ba3523ff3dcc608dc7e0c82428c24ab0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f51b86f4a3d4184b47e20297a62082644f98c8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatGPTClone.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatGPTClone.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
